f914c0ea2581ef23ff725ab2ebf37189e497185b
Carlos Arosemena <carlosarosemena@Carloss-MacBook-Pro.local>
2017-03-04 16:41:06 -0500

    try

--- src/main/java/com/amandaprettypizza/PizzaOrderSystemApplication.java
+++ src/main/java/com/amandaprettypizza/PizzaOrderSystemApplication.java
@@ -2,9 +2,17 @@ package com.amandaprettypizza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class PizzaOrderSystemApplication {
+	
+	@RequestMapping("/login")
+    @ResponseBody
+    String home() {
+      return "login";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaOrderSystemApplication.class, args);
